0e4ac63cd6f2c04b0d7d8ddbfa140ffb194cac50
Lucas Marques <lucas.msilva09@gmail.com>
2020-10-12 18:00:15 -0300

    Updating the migrations to generate the database in the first migration

--- ormconfig.js
+++ ormconfig.js
@@ -1,12 +1,12 @@
-[
+module.exports = [
   {
     "name": "default",
     "type": "postgres",
-    "host": "localhost",
-    "port": 5432,
-    "username": "postgres",
-    "password": "docker",
-    "database": "url_shortener",
+    "host": process.env.DATABASE_HOST,
+    "port": process.env.DATABASE_PORT,
+    "username": process.env.DATABASE_USERNAME,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_DATABASE,
     "entities": [
       "./src/modules/**/infra/typeorm/entities/*.ts"
 
@@ -18,4 +18,4 @@
       "migrationsDir": "./src/shared/infra/typeorm/migrations"
     }
   }
-]
+];
---
+++ src/shared/infra/typeorm/migrations/1000000000000-CreateDatabase.ts
@@ -0,0 +1,7 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateDatabase1000000000000 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> { }
+
+  public async down(queryRunner: QueryRunner): Promise<void> { }
+}
